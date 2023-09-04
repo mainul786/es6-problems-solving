e120da7791ca5910c6cf09fd599b2d568600fddd
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-09-04 23:47:18 +0530

    destrucring example

---
+++ destructuring.js
@@ -0,0 +1,16 @@
+const nayak = ['salman khan', 'amir khan', 'sohel khan', 'arbaj khan', 'kadir khan'];
+const [salman, amir, sohel, arbaj, kadir] = nayak;
+console.log(salman,amir);
+
+const phone = {
+    name : 'Infinix Hot 20 play',
+    model : 'X666',
+    color : 'violate',
+    price : 15000,
+    ram : '6gb',
+    networkType : '5G'
+};
+
+const {name, model, price} =  phone;
+console.log(name, model, price);
+
